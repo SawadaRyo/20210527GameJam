0a3e6bafa31352910ac8946f172003fd3ee2bafe
SR21C <rysawad@gmail.com>
2022-05-27 03:07:20 +0900

    Folder Add

---
+++ 20210527GameJam/Assets/Sawada/Scripts/Ranking.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ranking : SingletonBehaviour<Ranking>
+{
+    int[] m_scores = new int[10];
+    Text[] m_scoreText = new Text[10];
+    void Start()
+    {
+        ScoreSort(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ScoreSort(int score)
+    {
+        var n = m_scores.Length;
+
+        int h = n / 2;
+
+        while (h > 0)
+        {
+            for (int i = h; i < n; i++)
+            {
+                var k = i;
+
+                while (k >= h && score.CompareTo(m_scores[k - h]) > 0)
+                {
+                    m_scores[k] = m_scores[k - h];
+                    k -= h;
+                }
+
+                m_scores[k] = score;
+            }
+
+            h = h / 2;
+        }
+
+        for (int i = 0; i < m_scores.Length; i++)
+        {
+            m_scoreText[i].text = string
+        }
+    }
+}
---
+++ 20210527GameJam/Assets/Sawada/Scripts/SingletonBehaviour.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                Type t = typeof(T);
+                m_instance = (T)FindObjectOfType(t);
+                if (m_instance == null)
+                {
+                    Debug.LogError($"{t}をアタッチしているGameObjectがありません");
+                }
+            }
+            return m_instance;
+        }
+    }
+
+    private void Awake()
+    {
+        CheckIns();
+    }
+    bool CheckIns()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            return true;
+        }
+        else if (m_instance == this)
+        {
+            return true;
+        }
+        Destroy(this);
+        return false;
+    }
+}
+
